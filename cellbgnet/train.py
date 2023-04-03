d71c10dc36ef890130aac91e97045711ce4db737
Praneeth Karempudi <praneeth.karempudi@gmail.com>
2023-04-03 13:08:21 +0200

    Maually selected 20 beads...

--- cellbgnet/train.py
+++ cellbgnet/train.py
@@ -1,3 +1,22 @@
+import argparse
+import shutil
+import socket
+from pathlib import Path
+from datetime import datetime
+from art import tprint
+import numpy as np
+import matplotlib.pyplot as plt
+
+def parse_args():
+    pass
+
+def train_model():
+    pass
 
 def main():
-    print("Starting training ... ")
+    print("Starting cellbgnet training .... ")
+    tprint("CELLBGNET")
+
+
+if __name__ == '__main__':
+    main()
