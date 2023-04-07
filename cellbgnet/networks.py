9184ff808dcf86e9347f217ae68251c5feca248a
Praneeth Karempudi <praneeth.karempudi@gmail.com>
2023-04-07 22:36:48 +0200

    Network model seems done, understood all the parts

--- cellbgnet/networks.py
+++ cellbgnet/networks.py
@@ -5,6 +5,7 @@ import math
 from torch.optim.optimizer import Optimizer
 
 
+
 class CoordConv(nn.Module):
     """
     CoordConv class, input goes through conv layers,
@@ -23,6 +24,140 @@ class CoordConv(nn.Module):
         ret_2 = self.conv2d_coord(cell_bg_coord)
         return ret_1 + ret_2
 
+
+class OutnetBGConv(nn.Module):
+    """
+    Output from the U-net parts are transformed to the required number
+    for calculating loss or just inferring
+    """
+    def __init__(self, n_filters, pred_sig=False, pred_bg=False, 
+                 pad=1, ker_size=3, use_coordconv=False, device='cpu'):
+        super(OutnetBGConv, self).__init__()
+
+        self.pred_bg = pred_bg
+        self.pred_sig = pred_sig
+        self.use_coordconv = use_coordconv
+        self.device = device
+
+        if self.use_coordconv:
+            self.p_out1 = CoordConv(in_channels=n_filters, out_channels=n_filters, 
+                                    kernel_size=ker_size, padding=pad).to(device)
+            self.p_out2 = nn.Conv2d(in_channels=n_filters, out_channels=1, 
+                                    kernel_size=1, padding=0).to(device)
+            self.xyzi_out1 = CoordConv(in_channels=n_filters, out_channels=n_filters,
+                                    kernel_size=ker_size, padding=pad).to(device)
+            self.xyzi_out2 = nn.Conv2d(in_channels=n_filters, out_channels=4,
+                                    kernel_size=1, padding=0).to(device)
+            
+            
+            nn.init.kaiming_normal_(self.p_out1.conv2d_im.weight, mode='fan_in', nonlinearity='relu')
+            nn.init.kaiming_normal_(self.p_out1.conv2d_coord.weight, mode='fan_in', nonlinearity='relu')
+            nn.init.kaiming_normal_(self.p_out2.weight, mode='fan_in', nonlinearity='sigmoid')
+            nn.init.constant_(self.p_out2.bias, -6.)  # -6
+
+            nn.init.kaiming_normal_(self.xyzi_out1.conv2d_im.weight, mode='fan_in', nonlinearity='relu')
+            nn.init.kaiming_normal_(self.xyzi_out1.conv2d_coord.weight, mode='fan_in', nonlinearity='relu')
+            nn.init.kaiming_normal_(self.xyzi_out2.weight, mode='fan_in', nonlinearity='tanh')
+            nn.init.zeros_(self.xyzi_out2.bias)
+
+
+            if self.pred_sig:
+                self.xyzis_out1 = CoordConv(in_channels=n_filters, out_channels=n_filters,
+                                            kernel_size=ker_size, padding=pad).to(device)
+                self.xyzis_out2 = nn.Conv2d(in_channels=n_filters, out_channels=4, 
+                                            kernel_size=1, padding=0).to(device)
+
+                nn.init.kaiming_normal_(self.xyzis_out1.conv2d_im.weight, mode='fan_in', nonlinearity='relu')
+                nn.init.kaiming_normal_(self.xyzis_out1.conv2d_coord.weight, mode='fan_in', nonlinearity='relu')
+                nn.init.kaiming_normal_(self.xyzis_out2.weight, mode='fan_in', nonlinearity='sigmoid')
+                nn.init.zeros_(self.xyzis_out2.bias)
+
+            if self.pred_bg:
+                self.bg_out1 = CoordConv(in_channels=n_filters, out_channels=n_filters,
+                                         kernel_size=ker_size, padding=pad).to(device)
+                self.bg_out2 = nn.Conv2d(in_channels=n_filters, out_channels=1,
+                                         kernel_size=1, padding=0).to(device)
+
+                nn.init.kaiming_normal_(self.bg_out1.conv2d_im.weight, mode='fan_in', nonlinearity='relu')
+                nn.init.kaiming_normal_(self.bg_out1.conv2d_coord.weight, mode='fan_in', nonlinearity='relu')
+                nn.init.kaiming_normal_(self.bg_out2.weight, mode='fan_in', nonlinearity='sigmoid')
+                nn.init.zeros_(self.bg_out2.bias)
+
+        else:
+
+            self.p_out1 = nn.Conv2d(in_channels=n_filters, out_channels=n_filters, 
+                                    kernel_size=ker_size, padding=pad).to(device)
+            self.p_out2 = nn.Conv2d(in_channels=n_filters, out_channels=1,
+                                    kernel_size=1, padding=0).to(device)
+            self.xyzi_out1 = nn.Conv2d(in_channels=n_filters, out_channels=n_filters, 
+                                       kernel_size=ker_size, padding=pad).to(device)
+            self.xyzi_out2 = nn.Conv2d(in_channels=n_filters, out_channels=4,
+                                       kernel_size=1, padding=0).to(device)
+
+            nn.init.kaiming_normal_(self.p_out1.weight, mode='fan_in', nonlinearity='relu')
+            nn.init.kaiming_normal_(self.p_out2.weight, mode='fan_in', nonlinearity='sigmoid')
+            nn.init.constant_(self.p_out2.bias, -6.)  # -6
+
+            nn.init.kaiming_normal_(self.xyzi_out1.weight, mode='fan_in', nonlinearity='relu')
+            nn.init.kaiming_normal_(self.xyzi_out2.weight, mode='fan_in', nonlinearity='tanh')
+            nn.init.zeros_(self.xyzi_out2.bias)
+
+            if self.pred_sig:
+                self.xyzis_out1 = nn.Conv2d(in_channels=n_filters, out_channels=n_filters, 
+                                            kernel_size=ker_size, padding=pad).to(device)
+                self.xyzis_out2 = nn.Conv2d(in_channels=n_filters, out_channels=4,
+                                            kernel_size=1, padding=0).to(device)
+
+                nn.init.kaiming_normal_(self.xyzis_out1.weight, mode='fan_in', nonlinearity='relu')
+                nn.init.kaiming_normal_(self.xyzis_out2.weight, mode='fan_in', nonlinearity='sigmoid')
+                nn.init.zeros_(self.xyzis_out2.bias)
+
+            if self.pred_bg:
+                self.bg_out1 = nn.Conv2d(in_channels=n_filters, out_channels=n_filters, 
+                                         kernel_size=ker_size, padding=pad).to(device)
+                self.bg_out2 = nn.Conv2d(in_channels=n_filters, out_channels=1, 
+                                         kernel_size=1, padding=0).to(device)
+
+                nn.init.kaiming_normal_(self.bg_out1.weight, mode='fan_in', nonlinearity='relu')
+                nn.init.kaiming_normal_(self.bg_out2.weight, mode='fan_in', nonlinearity='sigmoid')
+                nn.init.zeros_(self.bg_out2.bias)
+
+
+    def forward(self, x, cell_bg_coord):
+        outputs = {}
+
+        if self.use_coordconv:
+            p = F.elu(self.p_out1(x, cell_bg_coord))
+            outputs['p'] = self.p_out2(p)
+
+            xyzi = F.elu(self.xyzi_out1(x, cell_bg_coord))
+            outputs['xyzi'] = self.xyzi_out2(xyzi)
+
+            if self.pred_sig:
+                xyzis = F.elu(self.xyzis_out1(x, cell_bg_coord))
+                outputs['xyzi_sig'] = self.xyzis_out2(xyzis)
+
+            if self.pred_bg:
+                bg = F.elu(self.bg_out1(x, cell_bg_coord))
+                outputs['bg'] = self.bg_out2(bg)
+        else:
+            p = F.elu(self.p_out1(x))
+            outputs['p'] = self.p_out2(p)
+
+            xyzi = F.elu(self.xyzi_out1(x))
+            outputs['xyzi'] = self.xyzi_out2(xyzi)
+
+            if self.pred_sig:
+                xyzis = F.elu(self.xyzis_out1(x))
+                outputs['xyzi_sig'] = self.xyzis_out2(xyzis)
+
+            if self.pred_bg:
+                bg = F.elu(self.bg_out1(x))
+                outputs['bg'] = self.bg_out2(bg)
+
+        return outputs
+
+
 class UnetBGConv(nn.Module):
     """
     Used for both frame analysis and contex module.. In our
@@ -31,7 +166,7 @@ class UnetBGConv(nn.Module):
     Cell Bg coordinate can be added on top if you set use_coordconv=True 
     """
     def __init__(self, n_inp, n_filters=64, n_stages=5, pad=1, ker_size=3,
-                  use_coordconv=False, device='cuda:0'):
+                  use_coordconv=False, device='cpu'):
         super(UnetBGConv, self).__init__()
         curr_N = n_filters
         self.n_stages = n_stages
@@ -58,25 +193,25 @@ class UnetBGConv(nn.Module):
 
         for i in range(n_stages):
             self.layer_path.append(
-                nn.Conv2d(in_channels=curr_N, out_channels=curr_N, kernel_size=2, stride=2, padding=0).cuda())
+                nn.Conv2d(in_channels=curr_N, out_channels=curr_N, kernel_size=2, stride=2, padding=0).to(device))
             self.layer_path.append(
-                nn.Conv2d(in_channels=curr_N, out_channels=curr_N * 2, kernel_size=ker_size, padding=pad).cuda())
+                nn.Conv2d(in_channels=curr_N, out_channels=curr_N * 2, kernel_size=ker_size, padding=pad).to(device))
             curr_N *= 2
             self.layer_path.append(
-                nn.Conv2d(in_channels=curr_N, out_channels=curr_N, kernel_size=ker_size, padding=pad).cuda())
+                nn.Conv2d(in_channels=curr_N, out_channels=curr_N, kernel_size=ker_size, padding=pad).to(device))
         
 
         for i in range(n_stages):
-            self.layer_path.append(nn.UpsamplingNearest2d(scale_factor=2).cuda())
+            self.layer_path.append(nn.UpsamplingNearest2d(scale_factor=2).to(device))
             self.layer_path.append(
-                nn.Conv2d(in_channels=curr_N, out_channels=curr_N // 2, kernel_size=ker_size, padding=pad).cuda())
+                nn.Conv2d(in_channels=curr_N, out_channels=curr_N // 2, kernel_size=ker_size, padding=pad).to(device))
 
             curr_N = curr_N // 2
 
             self.layer_path.append(
-                nn.Conv2d(in_channels=curr_N * 2, out_channels=curr_N, kernel_size=ker_size, padding=pad).cuda())
+                nn.Conv2d(in_channels=curr_N * 2, out_channels=curr_N, kernel_size=ker_size, padding=pad).to(device))
             self.layer_path.append(
-                nn.Conv2d(in_channels=curr_N, out_channels=curr_N, kernel_size=ker_size, padding=pad).cuda())
+                nn.Conv2d(in_channels=curr_N, out_channels=curr_N, kernel_size=ker_size, padding=pad).to(device))
 
 
         for m in self.layer_path:
